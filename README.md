f4775beaac6b7d3eb353abb2fa14fbcc69e79899
Erik Svendsen Laustsen <erikdrums@gmail.com>
2015-12-09 14:27:24 +0100

    Working with configfile in home dir.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # wplive2local
+
+You need python package 'bunch' installed
+
+You need to create a configuration file in your home directory named
+.wplive2local_conf
+
+in the format:
+
+# Local settings
+local:
+    mysql: 'mysql'
+    mysqldump: 'mysqldump'
+    db_user: 'root'
+    db_password: 'root'
+    site_password: 'password'
+
+
+sites:
+    jyllandsakvariet:
+        local:
+            db: 'dbname'
+            url: 'http://localhost/amazingsite.com'
+            backup_path: '/tmp/amazingsite_backup'
+        production:
+            ssh_user: 'root'
+            ssh_address: 'http://amazingsite.com'
+            db_user: 'root'
+            db_password: 'password'
+            host: 'localhost'
+            url: 'http://amazingsite.com'
+            db: 'dbname'
+
--- wplive2local.py
+++ wplive2local.py
@@ -7,80 +7,97 @@ import syslog
 import errno
 import logging
 import subprocess
+import yaml
+from bunch import Bunch
 
 def main(argv):
-
-    print "Begin Migration"
+    HOME_PATH = os.path.expanduser('~')
+    CONFIG_PATH = os.path.join(HOME_PATH, '.wplive2local_conf')
     initial = False
 
-    production_ssh_user = ''
-    production_ssh_address = ''
-    production_db_user = ''
-    production_db_password = ''
-    production_host = ''
-    production_url = ''
-    production_db = ''
-    dbbackup_path = ''
+    try:
+        configurations = yaml.load(open(CONFIG_PATH).read())
+        conf = Bunch.fromDict(configurations)
+        local = conf.local
+        sites = conf.sites
+    except IOError:
+        print "NO SETTINGS FILE"
+        sys.exit()    
+    print argv, argv[0]
+    try:
+        site = argv[0]
+        site_conf = sites[site]
+        site_local = site_conf.local
+        site_production = site_conf.production
+    except KeyError:
+        print "This is not a configured site"
+        sys.exit()    
 
-    local_mysql = ''
-    local_mysqldump = ''
-    local_db_user = ''
-    local_db_password = ''
-    local_db = ''
-    local_site_password = ''
-    local_url = ''
+    print local
+    print "Begin Migration"
 
-    def build_query(user, password, runtype='mysql', host='localhost', database=None):
+
+    def _build_query(user, password, runtype='mysql', host='localhost', database=None):
         if database is None:
             database = ''
         return "{} -u{} -p{} {} -h{}".format(runtype, user, password, database, host)
 
-    def build_query_command(user, password, query_command, runtype='mysql', host='localhost', database=None):
-        query = build_query(user, password, runtype, host, database)
+    def _build_query_command(user, password, query_command, runtype='mysql', 
+                                                    host='localhost', database=None):
+        query = _build_query(user, password, runtype, host, database)
         return "{} -e '{}'".format(query, query_command)
 
 
-    def run_query(user, password, query, runtype='mysql', host='localhost', database=None):
-        os.popen(build_query_command(user, password, query, runtype, host, database))
+    def _run_query(user, password, query, runtype='mysql', host='localhost', database=None):
+        print _build_query_command(user, password, query, runtype, host, database)
+        os.popen(_build_query_command(user, password, query, runtype, host, database))
 
     def local_db_dump():
-        dump_query = " --opt -c " + local_db + " > " + dbbackup_path + ".sql"
-        query_command = "{} -e --opt -c {} > {}.sql".format(build_query(local_db_user, local_db_password, local_mysqldump), 
-                                                                        local_db, dbbackup_path)
+        dump_query = " --opt -c " + site_local.db + " > " + site_local.backup_path + ".sql"
+        query_command = "{} -e --opt -c {} > {}.sql".format(
+                    _build_query(local.db_user, local.db_password, local.mysqldump), 
+                                                site_local.db, site_local.backup_path)
         os.popen(query_command)
     
     def local_db_drop():
-        dump_query = "drop database {}".format(local_db)
-        run_query(local_db_user, local_db_password, dump_query, local_mysql)    
+        dump_query = "drop database {}".format(site_local.db)
+        _run_query(local.db_user, local.db_password, dump_query, local.mysql)    
 
     def local_db_create():
-        dump_query = "create database {}".format(local_db)
-        run_query(local_db_user, local_db_password, dump_query, local_mysql)    
+        dump_query = "create database {}".format(site_local.db)
+        _run_query(local.db_user, local.db_password, dump_query, local.mysql)    
 
     
     def production_dump_to_local():
-        production_query = build_query(production_db_user, production_db_password, 'mysqldump', database=production_db)
-        local_query = build_query(local_db_user, local_db_password, local_mysql, database=local_db)
+        production_query = _build_query(site_production.db_user, 
+            site_production.db_password, 'mysqldump', database=site_production.db)
+        local_query = _build_query(local.db_user, local.db_password, 
+                                                    local.mysql, database=site_local.db)
         command = "ssh {}@{} '{} | gzip -9' | gzip -d | {}".format(
-            production_ssh_user, production_ssh_address, production_query, local_query)
+            site_production.ssh_user, site_production.ssh_address, production_query, 
+                                                                        local_query)
         
         os.popen(command)
 
 
     def local_update_urls():
-        query = build_query(local_db_user, local_db_password, local_mysql, database=local_db)
-        query_command = """ "UPDATE wp_options SET option_value = replace(option_value, '{0}', '{1}')\
+        query = _build_query(local.db_user, local.db_password, local.mysql, 
+                                                                database=site_local.db)
+        query_command = """ "UPDATE wp_options \
+            SET option_value = replace(option_value, '{0}', '{1}')\
             WHERE option_name = 'home' OR option_name = 'siteurl';\
             UPDATE wp_posts SET guid = replace(guid, '{0}', '{1}');\
             UPDATE wp_posts SET post_content = replace(post_content, '{0}', '{1}');\
             UPDATE wp_postmeta SET meta_value = replace(meta_value, '{0}', '{1}');";\
-        """.format(production_url, local_url)
+        """.format(site_production.url, site_local.url)
         os.popen("{} -Bse {}".format(query, query_command))    
 
 
     def local_set_password():
-        query = """UPDATE wp_users SET user_pass = MD5("{}") WHERE ID=1 LIMIT 1;""".format(local_site_password, '{}')
-        run_query(local_db_user, local_db_password, query, local_mysql, database=local_db)   
+        query = """UPDATE wp_users SET user_pass = MD5("{}") WHERE ID=1 LIMIT 1;
+                                                """.format(local.site_password, '{}')
+        _run_query(local.db_user, local.db_password, query, local.mysql, 
+                                                                database=site_local.db)   
 
     if '--init' in argv:
         initial = True
@@ -95,6 +112,6 @@ def main(argv):
     local_set_password()
 
     print "SUCCESS"
-
+ 
 if __name__ == "__main__":
   main(sys.argv[1:])
